3795ce24756e0d88b82f5b920d229a796c8040eb
MiguelAngeldevlp <miguel.angel01moya@gmail.com>
2022-11-17 18:55:50 +0100

    LOK

---
+++ main.js
@@ -0,0 +1,28 @@
+////////////KATA1
+// function multiplyByEigth(number) {
+//  if (number % 2 === 0) {
+//     number*=8
+//    } 
+//    else if(number % 2 === 1){
+//     number*=9
+//    }
+// }
+// number(4);
+// console.log();
+
+
+
+
+//////////KATA2
+// function sumByNum(arr) {
+//     let cont = 0
+//     for (let i = 0; i <=arr; i++) {
+//         cont += i
+//     }return cont
+// }
+
+// console.log(sumByNum(7));
+
+
+
+/////KATA3
